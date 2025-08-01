cc0d80f46caea3417ddcb69f131f27a78cdb074c
Nebu Placid <160344350+iamNebu@users.noreply.github.com>
2025-08-01 18:55:50 +0530

    Update music_world.java

--- music_world.java
+++ music_world.java
@@ -30,111 +30,111 @@ public class GUI {
     private static Connection con = null;
 
     GUI() {
-        frame = new JFrame("GUI_PROJECT"); [cite: 291]
+        frame = new JFrame("GUI_PROJECT");
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(500, 500); [cite: 294]
-        frame.setLayout(new BorderLayout()); [cite: 295]
+        frame.setSize(500, 500);
+        frame.setLayout(new BorderLayout());
         frame.setVisible(true);
-        frame.add(Jpanel_1, BorderLayout.CENTER); [cite: 297]
+        frame.add(Jpanel_1, BorderLayout.CENTER);
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver"); [cite: 299]
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
         try {
-            con = DriverManager.getConnection(url, username, password); [cite: 303]
+            con = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         // Action Listener for the Delete Button
-        deleteButton.addActionListener(e -> { [cite: 306]
-            Integer id = Integer.valueOf(textField1.getText()); [cite: 307]
+        deleteButton.addActionListener(e -> {
+            Integer id = Integer.valueOf(textField1.getText());
             String sql = "DELETE FROM music WHERE id = ?";
             try (PreparedStatement prst = con.prepareStatement(sql)) {
-                prst.setInt(1, id); [cite: 310]
-                int affectedRows = prst.executeUpdate(); [cite: 311]
+                prst.setInt(1, id);
+                int affectedRows = prst.executeUpdate();
                 if (affectedRows > 0) {
-                    JOptionPane.showMessageDialog(null, "Successfully deleted", "Success", JOptionPane.INFORMATION_MESSAGE); [cite: 313]
+                    JOptionPane.showMessageDialog(null, "Successfully deleted", "Success", JOptionPane.INFORMATION_MESSAGE);
                 } else {
-                    JOptionPane.showMessageDialog(null, "Failed to delete", "Failure", JOptionPane.INFORMATION_MESSAGE); [cite: 315]
+                    JOptionPane.showMessageDialog(null, "Failed to delete", "Failure", JOptionPane.INFORMATION_MESSAGE);
                 }
             } catch (SQLException er) {
                 er.printStackTrace();
             } finally {
-                textField1.setText(""); [cite: 319]
-                textField4.setText(""); [cite: 320]
-                textField6.setText(""); [cite: 321]
-                textField5.setText(""); [cite: 322]
+                textField1.setText("");
+                textField4.setText("");
+                textField6.setText("");
+                textField5.setText("");
             }
         });
 
         // Action Listener for the Insert Button
-        Insert_Button.addActionListener(e -> { [cite: 323]
-            String name = textField4.getText(); [cite: 324]
-            String artist = textField6.getText(); [cite: 325]
-            String genre = textField5.getText(); [cite: 326]
-            String sql = "INSERT INTO music (name, genre, artist) VALUES (?, ?, ?);"; [cite: 327]
+        Insert_Button.addActionListener(e -> {
+            String name = textField4.getText();
+            String artist = textField6.getText();
+            String genre = textField5.getText();
+            String sql = "INSERT INTO music (name, genre, artist) VALUES (?, ?, ?);";
             try (PreparedStatement prst = con.prepareStatement(sql)) {
-                prst.setString(1, name); [cite: 329]
-                prst.setString(2, genre); [cite: 330]
-                prst.setString(3, artist); [cite: 331]
+                prst.setString(1, name);
+                prst.setString(2, genre);
+                prst.setString(3, artist);
                 int affectedRows = prst.executeUpdate();
                 if (affectedRows > 0) {
-                    JOptionPane.showMessageDialog(null, "Successfully Inserted", "Success", JOptionPane.INFORMATION_MESSAGE); [cite: 334]
+                    JOptionPane.showMessageDialog(null, "Successfully Inserted", "Success", JOptionPane.INFORMATION_MESSAGE);
                 } else {
-                    JOptionPane.showMessageDialog(null, "Failed to Insert", "Failure", JOptionPane.INFORMATION_MESSAGE); [cite: 336]
+                    JOptionPane.showMessageDialog(null, "Failed to Insert", "Failure", JOptionPane.INFORMATION_MESSAGE);
                 }
             } catch (SQLException er) {
                 er.printStackTrace();
             } finally {
-                textField1.setText(""); [cite: 340]
-                textField4.setText(""); [cite: 342]
-                textField6.setText(""); [cite: 344]
-                textField5.setText(""); [cite: 345]
+                textField1.setText("");
+                textField4.setText("");
+                textField6.setText("");
+                textField5.setText("");
             }
         });
 
         // Action Listener for the Update Button
-        updateButton.addActionListener(e -> { [cite: 346]
-            Integer id = Integer.valueOf(textField1.getText()); [cite: 347]
-            String name = textField4.getText(); [cite: 348]
-            String artist = textField6.getText(); [cite: 349]
-            String genre = textField5.getText(); [cite: 350]
+        updateButton.addActionListener(e -> {
+            Integer id = Integer.valueOf(textField1.getText());
+            String name = textField4.getText();
+            String artist = textField6.getText();
+            String genre = textField5.getText();
             String sql = "UPDATE music SET name = ?, genre = ?, artist = ? WHERE id = ?";
             try (PreparedStatement prst = con.prepareStatement(sql)) {
-                prst.setString(1, name); [cite: 353]
-                prst.setString(2, genre); [cite: 354]
-                prst.setString(3, artist); [cite: 355]
-                prst.setInt(4, id); [cite: 356]
+                prst.setString(1, name);
+                prst.setString(2, genre);
+                prst.setString(3, artist);
+                prst.setInt(4, id);
                 int affectedRows = prst.executeUpdate();
                 if (affectedRows > 0) {
-                    JOptionPane.showMessageDialog(null, "Successfully updated", "Success", JOptionPane.INFORMATION_MESSAGE); [cite: 359]
+                    JOptionPane.showMessageDialog(null, "Successfully updated", "Success", JOptionPane.INFORMATION_MESSAGE);
                 } else {
-                    JOptionPane.showMessageDialog(null, "Failed to Update", "Failure", JOptionPane.INFORMATION_MESSAGE); [cite: 360]
+                    JOptionPane.showMessageDialog(null, "Failed to Update", "Failure", JOptionPane.INFORMATION_MESSAGE);
                 }
             } catch (SQLException er) {
                 er.printStackTrace();
             } finally {
-                textField1.setText(""); [cite: 364]
-                textField4.setText(""); [cite: 365]
-                textField6.setText(""); [cite: 366]
-                textField5.setText(""); [cite: 368]
+                textField1.setText("");
+                textField4.setText("");
+                textField6.setText("");
+                textField5.setText("");
             }
         });
 
         // Action Listener for the View Button
-        viewButton.addActionListener(e -> { [cite: 369]
+        viewButton.addActionListener(e -> {
             try {
-                Statement st = con.createStatement(); [cite: 370]
-                String query = "SELECT * FROM music;"; [cite: 371]
+                Statement st = con.createStatement();
+                String query = "SELECT * FROM music;";
                 ResultSet rs = st.executeQuery(query);
                 ResultSetMetaData rsmd = rs.getMetaData();
                 DefaultTableModel model = (DefaultTableModel) tblData.getModel();
                 
-                int columnCount = rsmd.getColumnCount(); [cite: 372]
+                int columnCount = rsmd.getColumnCount();
                 String[] columnNames = new String[columnCount];
                 for (int i = 0; i < columnCount; i++) {
                     columnNames[i] = rsmd.getColumnName(i + 1);
@@ -143,12 +143,12 @@ public class GUI {
 
                 model.setRowCount(0); // Clear existing data
 
-                while (rs.next()) { [cite: 376]
+                while (rs.next()) {
                     Object[] rowData = new Object[columnCount];
                     for (int i = 0; i < columnCount; i++) {
-                        rowData[i] = rs.getObject(i + 1); [cite: 379]
+                        rowData[i] = rs.getObject(i + 1);
                     }
-                    model.addRow(rowData); [cite: 380]
+                    model.addRow(rowData);
                 }
                 st.close();
                 rs.close();
@@ -158,21 +158,21 @@ public class GUI {
         });
 
         // Action Listener for the Clear Button
-        clr_button.addActionListener(new ActionListener() { [cite: 386]
+        clr_button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                textField1.setText(""); [cite: 388]
-                textField4.setText(""); [cite: 389]
-                textField6.setText(""); [cite: 391]
-                textField5.setText(""); [cite: 392]
-                tblData.setModel(new DefaultTableModel()); [cite: 393]
+                textField1.setText("");
+                textField4.setText("");
+                textField6.setText("");
+                textField5.setText("");
+                tblData.setModel(new DefaultTableModel());
             }
         });
     }
 
-    public static void main(String[] args) { [cite: 394]
-        SwingUtilities.invokeLater(new Runnable() { [cite: 395]
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new GUI(); [cite: 396]
+                new GUI();
             }
         });
     }
